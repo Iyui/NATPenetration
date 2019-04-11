91ebb84bc0e120f898683a9de2ccf67c405f100d
Iyui <314754801@qq.com>
2019-04-11 16:21:50 +0800

    init

---
+++ NATPenetration.Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace NATPenetration.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "115.21.X.X";//服务端IP地址
+            int port = 555;
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //设置端口可复用
+            clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //连接服务端
+            clientSocket.Connect(host, port);
+            Console.WriteLine("Connect：" + host + "  " + port);
+
+            string data = "hello,Server!";
+            clientSocket.Send(Encoding.ASCII.GetBytes(data));
+            Console.WriteLine("Send：" + data);
+            byte[] recBytes = new byte[100];
+            //获取到双方的ip及端口号
+            int bytes = clientSocket.Receive(recBytes, recBytes.Length, 0);
+            string result = Encoding.ASCII.GetString(recBytes, 0, bytes);
+            Console.WriteLine("Recv：" + result);
+            clientSocket.Close();
+
+            string[] ips = result.Split(':');
+            int myPort = Convert.ToInt32(ips[1]);
+            string otherIp = ips[2];
+            int otherPort = Convert.ToInt32(ips[3]);
+
+
+            Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            mySocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //绑定到之前连通过的端口号
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, Convert.ToInt32(myPort));
+            mySocket.Bind(ipe);
+            //尝试5次连接
+            for (int j = 0; j < 5; j++)
+            {
+                try
+                {
+                    mySocket.Connect(otherIp, otherPort);
+                    Console.WriteLine("Connect：成功{0},{1}", otherIp, otherPort);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Connect：失败");
+                    // otherPort++;//如果是对称NAT，则有可能客户端的端口号已经改变，正常有规律的应该是顺序加1，可以尝试+1再试（我使用手机热点连接的时候端口号就变成+1的了）除非是碰到随机端口，那就不行了。
+                }
+
+            }
+            while (true)
+            {
+                mySocket.Send(Encoding.ASCII.GetBytes("hello,the other client!"));
+
+                byte[] recv = new byte[4096];
+                int len = mySocket.Receive(recv, recv.Length, 0);
+                result = Encoding.ASCII.GetString(recv, 0, len);
+                Console.WriteLine("recv :" + result);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
--- NATPenetration/Program.cs
+++ NATPenetration/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 namespace NATPenetration
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int port = 555;
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, port);
+            Socket sSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sSocket.Bind(ipe);
+            sSocket.Listen(100);
+            Console.WriteLine("监听已经打开，请等待");
+
+            while (true)
+            {
+                Socket serverSocket1 = sSocket.Accept();
+                Console.WriteLine("连接已经建立");
+                string recStr = "";
+                byte[] recByte = new byte[4096];
+                int bytes = serverSocket1.Receive(recByte);
+                IPEndPoint ep1 = (IPEndPoint)serverSocket1.RemoteEndPoint;
+                Console.WriteLine(" from {0}", ep1.ToString());
+                recStr = Encoding.ASCII.GetString(recByte, 0, bytes);
+                Console.WriteLine("客户端1:{0}", recStr);
+
+                Socket serverSocket2 = sSocket.Accept();
+                bytes = serverSocket2.Receive(recByte);
+                IPEndPoint ep2 = (IPEndPoint)serverSocket2.RemoteEndPoint;
+                Console.WriteLine(" from {0}", ep2.ToString());
+                recStr = Encoding.ASCII.GetString(recByte, 0, bytes);
+                Console.WriteLine("客户端2:{0}", recStr);
+
+
+                byte[] sendByte = Encoding.ASCII.GetBytes(ep1.ToString() + ":" + ep2.ToString());
+                serverSocket1.Send(sendByte, sendByte.Length, 0);
+
+                sendByte = Encoding.ASCII.GetBytes(ep2.ToString() + ":" + ep1.ToString());
+                serverSocket2.Send(sendByte, sendByte.Length, 0);
+
+                serverSocket1.Close();
+                serverSocket2.Close();
+            }
+
         }
     }
 }
