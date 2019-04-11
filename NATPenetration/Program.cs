69b626513c1fbd099412661e1b0b73ec1142493a
Iyui <314754801@qq.com>
2019-04-11 16:17:27 +0800

    添加项目文件。

---
+++ NATPenetration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NATPenetration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
